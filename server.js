1bbd6dd94c86f65880029c6952f3a7758d42dd27
ammarelazazi-design <ammarelazazi@gmail.com>
2026-04-10 18:56:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -90,6 +90,6 @@ app.get('/health', (req, res) => res.send("ELAZ Bot is LIVE! ✅"));
 
 // تشغيل السيرفر
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`🚀 السيرفر شغال على بورت ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
 });
